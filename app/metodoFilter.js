385dcdcaaa200d855c25f358ef4d4cc1d4650a44
Natan Rocha <natan.rocha@codeandsoul.com>
2024-03-06 11:40:24 -0300

    added a function that shows the total value of available books

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,28 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel'?livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtarPorDisponibilidade() : filtarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        const valorTotal = calcularValortotalDeLivrosDisponiveis(livrosFiltrados);
+
+        exibirValorTotalDosLIvrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+
+function filtarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+function filtarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function exibirValorTotalDosLIvrosDisponiveisNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+    
 }
